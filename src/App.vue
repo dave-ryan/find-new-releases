2124757898d34ddc36f93f02431730b7c988854f
dave-a-ryan <daveryan145@gmail.com>
2023-02-09 21:43:01 -0600

    huge changes, really huge, the best changes. functioning search with limited results

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <template>
   <div class="">
-    <button @click="testquery" class="btn btn-primary">Click Me!</button>
-    <div>
-      {{ output }}
+    <form @submit.prevent="testquery">
+      <div class="mb-3">
+        <label for="queryinput" class="form-label"
+          >Please enter an artist name</label
+        >
+        <input
+          type="text"
+          v-model="query"
+          class="form-control"
+          id="queryinput"
+          placeholder="example: interpol"
+        />
+      </div>
+      <button class="btn btn-primary" type="submit">Search!</button>
+    </form>
+
+    <div v-for="album in albums" :key="album.id" class="mt-3">
+      {{ album.title }}, {{ album.year }}
+      <a
+        :href="'https://www.discogs.com/master/' + album.master_id"
+        target="_blank"
+      >
+        <img :src="album.thumb" alt="" />
+      </a>
     </div>
   </div>
 </template>
@@ -21,19 +42,31 @@
 <script>
 import axios from "axios";
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
   data: function () {
-    return { ouput: "test" };
+    return { albums: [], query: "" };
   },
   methods: {
     testquery: function () {
       axios
-        .get("https://api.discogs.com/artists/1/releases?page=2&per_page=75")
+        .get(
+          `https://api.discogs.com/database/search?type=master&artist=${this.query.replace(
+            /\s/g,
+            "+"
+          )}&format=album&token=vwbBWENsiqyNVUgWWlunGyTKRxXHVSHJNHosZUyj`,
+          {
+            // headers: {
+            //   "user-agent":
+            //     "FindMissingAlbums/1.0 +http://www.findyouralbums.com",
+            // },
+          }
+        )
         .then((response) => {
-          this.output = response.data;
+          this.albums = response.data.results.filter((release) =>
+            release.title.toLowerCase().includes(this.query.toLowerCase())
+          );
+          this.albums = this.albums.sort((a, b) => {
+            return parseInt(a.year) > parseInt(b.year) ? 1 : -1;
+          });
           console.log(response);
         });
     },
