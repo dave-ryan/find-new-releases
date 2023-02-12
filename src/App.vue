99e62029a57ee60d8786d41b69e7c69f506f6467
dave-a-ryan <daveryan145@gmail.com>
2023-02-12 00:28:51 -0600

    added in csv upload

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,45 @@
 <template>
   <div class="">
-    <form @submit.prevent="testquery">
-      <div class="mb-3">
-        <label for="queryinput" class="form-label"
-          >Please enter an artist name</label
-        >
-        <input
-          type="text"
-          v-model="query"
-          class="form-control"
-          id="queryinput"
-          placeholder="example: interpol"
-        />
+    <div class="row">
+      <div class="col-2"></div>
+      <div class="col submission-column">
+        <form @submit.prevent="testquery">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text">Artist Search</span>
+            </div>
+            <input
+              type="text"
+              v-model="query"
+              class="form-control"
+              id="queryinput"
+              placeholder="example: interpol"
+            />
+            <div class="input-group-apend">
+              <button class="btn btn-primary" type="submit">Search!</button>
+            </div>
+          </div>
+        </form>
+
+        <div class="input-group mb-3">
+          <input
+            type="file"
+            class="form-control"
+            id="inputGroupFile04"
+            aria-describedby="inputGroupFileAddon04"
+            aria-label="Upload"
+          />
+          <button
+            class="btn btn-outline-primary"
+            type="button"
+            id="inputGroupFileAddon04"
+          >
+            Upload CSV
+          </button>
+        </div>
       </div>
-      <button class="btn btn-primary" type="submit">Search!</button>
-    </form>
+      <div class="col-2"></div>
+    </div>
 
     <div v-for="album in albums" :key="album.id" class="mt-3">
       {{ album.title }}, {{ album.year }}
