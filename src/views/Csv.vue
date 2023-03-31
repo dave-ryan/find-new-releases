bbd263162da9f6f53e7279cd47d81159ce63a70e
dave-a-ryan <daveryan145@gmail.com>
2023-03-31 11:36:36 -0500

    moved everything inside a modal

--- src/views/Csv.vue
+++ src/views/Csv.vue
@@ -1,11 +1,50 @@
 <template>
   <div class="container text-center mt-5">
     <div class="row">
-      <div class="col-2"></div>
       <div class="col submission-column">
-        <div class="row">
-          <div class="col-12">
-            <div>
+        <div>
+          <button
+            type="button"
+            class="btn btn-lg btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#upload-modal"
+            @click="this.uploadstatus = 1"
+          >
+            Begin Upload
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <!--  Modal -->
+    <div
+      class="modal fade modal-xl"
+      id="upload-modal"
+      tabindex="-1"
+      aria-labelledby="modalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <div
+              class="modal-title fs-5"
+              id="modalLabel"
+              v-if="uploadedFileName.length > 0"
+            >
+              File Uploaded:
+              <span class="font-monospace"> {{ uploadedFileName }}</span>
+            </div>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <!-- UPLOAD START -->
+            <div v-if="uploadstatus === 1">
               <form class="mt-4">
                 <label for="csvUpload" class="btn btn-lg btn-outline-primary">
                   Upload CSV
@@ -19,129 +58,155 @@
                   v-focus
                 />
               </form>
-              <button class="btn btn-sm btn-danger m-1" @click="clear">
-                clear
-              </button>
-              <div class="font-monospace mt-3">{{ uploadedFileName }}</div>
-              <div>{{ csvdata }}</div>
+            </div>
+
+            <!-- HEADER MANIPULATION -->
+            <div v-if="uploadstatus === 2">
+              <div v-for="header in csvheaders" :key="header">
+                <div class="dropdown m-2">
+                  <button
+                    class="btn btn-secondary dropdown-toggle"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    {{ header }}
+                  </button>
+                  <ul class="dropdown-menu">
+                    <li v-for="trueheader in trueheaders" :key="trueheader">
+                      <div
+                        class="dropdown-item"
+                        @click="headerchange(trueheader, header)"
+                      >
+                        {{ trueheader }}
+                      </div>
+                    </li>
+                  </ul>
+                  <i
+                    v-if="this.trueheaders.includes(header)"
+                    class="bi bi-check-circle-fill text-success ms-2"
+                  ></i>
+                  <i
+                    v-if="!this.trueheaders.includes(header)"
+                    class="bi bi-x-circle-fill text-danger ms-2"
+                  ></i>
+                </div>
+              </div>
+            </div>
+
+            <div v-if="uploadstatus === 3">
+              <table class="table table-bordered table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col" v-for="header in csvheaders" :key="header">
+                      {{ header }}
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                  <tr
+                    v-for="(row, index) in csvdata"
+                    :key="row.ExternalId"
+                    class="mt-3"
+                  >
+                    <td>
+                      <input
+                        v-if="this.editing === `${index}, FirstName`"
+                        v-model="row.FirstName"
+                        type="text"
+                        @blur="editing = false"
+                        @keyup.enter="editing = false"
+                        v-focus
+                      />
+                      <div
+                        v-if="this.editing !== `${index}, FirstName`"
+                        @click="this.editing = `${index}, FirstName`"
+                      >
+                        {{ row.FirstName }}
+                      </div>
+                    </td>
+                    <td>
+                      <input
+                        v-if="this.editing === `${index}, LastName`"
+                        v-model="row.LastName"
+                        type="text"
+                        @blur="editing = false"
+                        @keyup.enter="editing = false"
+                        v-focus
+                      />
+                      <div
+                        v-if="this.editing !== `${index}, LastName`"
+                        @click="this.editing = `${index}, LastName`"
+                      >
+                        {{ row.LastName }}
+                      </div>
+                    </td>
+                    <td>
+                      <input
+                        v-if="this.editing === `${index}, Email`"
+                        v-model="row.Email"
+                        type="text"
+                        @blur="editing = false"
+                        @keyup.enter="editing = false"
+                        v-focus
+                      />
+                      <div
+                        v-if="this.editing !== `${index}, Email`"
+                        @click="this.editing = `${index}, Email`"
+                      >
+                        {{ row.Email }}
+                      </div>
+                    </td>
+                    <td>
+                      <input
+                        v-if="this.editing === `${index}, ExternalId`"
+                        v-model="row.ExternalId"
+                        type="text"
+                        @blur="editing = false"
+                        @keyup.enter="editing = false"
+                        v-focus
+                      />
+                      <div
+                        v-if="this.editing !== `${index}, ExternalId`"
+                        @click="this.editing = `${index}, ExternalId`"
+                      >
+                        {{ row.ExternalId }}
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
-        </div>
-      </div>
-
-      <div class="col-2"></div>
-    </div>
-
-    <div class="row mt-5">
-      <div class="col-12">
-        <h1>Headers</h1>
-        <div v-for="header in csvheaders" :key="header">
-          <div class="dropdown m-2">
-            <button
-              class="btn btn-secondary dropdown-toggle"
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              {{ header }}
-            </button>
-            <ul class="dropdown-menu">
-              <li v-for="trueheader in trueheaders" :key="trueheader">
-                <div
-                  class="dropdown-item"
-                  @click="headerchange(trueheader, header)"
-                >
-                  {{ trueheader }}
-                </div>
-              </li>
-            </ul>
-            <i
-              v-if="this.trueheaders.includes(header)"
-              class="bi bi-check-circle-fill text-success ms-2"
-            ></i>
-            <i
-              v-if="!this.trueheaders.includes(header)"
-              class="bi bi-x-circle-fill text-danger ms-2"
-            ></i>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row mt-5">
-      <div class="col-12">
-        <table class="table table-bordered table-hover">
-          <thead>
-            <tr>
-              <th scope="col" v-for="header in csvheaders" :key="header">
-                {{ header }}
-              </th>
-            </tr>
-          </thead>
-          <tbody class="table-group-divider">
-            <tr
-              v-for="(row, index) in csvdata"
-              :key="row.ExternalId"
-              class="mt-3"
-            >
-              <td>
-                <input
-                  v-if="this.editing === `${index}, FirstName`"
-                  v-model="row.FirstName"
-                  type="text"
-                  @blur="editing = false"
-                  @keyup.enter="editing = false"
-                  v-focus
-                />
-                <div
-                  v-if="this.editing !== `${index}, FirstName`"
-                  @click="this.editing = `${index}, FirstName`"
-                >
-                  {{ row.FirstName }}
-                </div>
-              </td>
-              <td>
-                {{ row.LastName }}
-              </td>
-              <td>
-                {{ row.Email }}
-              </td>
-              <td>
-                {{ row.ExternalId }}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-    <!--  Modal -->
-    <div
-      class="modal fade"
-      id="modal"
-      tabindex="-1"
-      aria-labelledby="modalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="modalLabel">Title</h1>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body"></div>
           <div class="modal-footer">
             <button
               type="button"
-              class="btn btn-secondary"
-              data-bs-dismiss="modal"
+              class="btn btn-outline-danger"
+              v-if="this.uploadstatus > 1"
+              @click="this.uploadstatus -= 1"
             >
-              Close
+              Previous
             </button>
+            <button
+              type="button"
+              class="btn btn-outline-success"
+              v-if="rawdata.length > 0 && this.uploadstatus < 3"
+              @click="this.uploadstatus += 1"
+            >
+              Next
+            </button>
+            <a
+              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+              target="_blank"
+            >
+              <button
+                type="button"
+                class="btn btn-success"
+                v-if="this.uploadstatus === 3"
+              >
+                Complete Feed Upload!
+              </button>
+            </a>
           </div>
         </div>
       </div>
@@ -164,6 +229,7 @@ export default {
       rawdata: [],
       uploadedFileName: "",
       editing: [],
+      uploadstatus: 0,
     };
   },
   computed: {},
@@ -202,6 +268,7 @@ export default {
       this.csvdata = [];
       this.csvheaders = [];
       this.uploadedFileName = "";
+      this.uploadstatus = 1;
     },
     handleFileChange(e) {
       if (e.target.files && e.target.files[0]) {
